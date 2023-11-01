e7e6f65b99666d2948d71e15fe6a544ad74b761b
aniltepe <nazimatepe@gmail.com>
2023-11-02 02:05:54 +0300

    clean up 20231102

--- test_kernel.py
+++ test_kernel.py
@@ -1,46 +1,19 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from skimage.io import imread, imshow
-from skimage.color import rgb2gray
-from skimage.transform import rescale
+import cv2 as cv
 from scipy.signal import convolve2d
+from skimage.transform import rescale
+from skimage.color import rgb2gray
+from skimage.util import img_as_ubyte, img_as_float
 
-my_dog = imread('figure.png')
-my_dog_gray = rgb2gray(my_dog[:,:,:3])
-# Edge Detection1
-kernel1 = np.array([[0, -1, 0],
-                    [-1, 4, -1],
-                    [0, -1, 0]])
-# Edge Detection2
-kernel2 = np.array([[-1, -1, -1],
-                    [-1, 8, -1],
-                    [-1, -1, -1]])
-# Bottom Sobel Filter
-kernel3 = np.array([[-1, -2, -1],
-                    [0, 0, 0],
-                    [1, 2, 1]])
-# Top Sobel Filter
-kernel4 = np.array([[1, 2, 1],
-                    [0, 0, 0],
-                    [-1, -2, -1]])
-# Left Sobel Filter
-kernel5 = np.array([[1, 0, -1],
-                    [2, 0, -2],
-                    [1, 0, -1]])
-# Right Sobel Filter
-kernel6 = np.array([[-1, 0, 1],
-                    [-2, 0, 2],
-                    [-1, 0, 1]])
-kernels = [kernel1, kernel2, kernel3, kernel4, kernel5, kernel6]
-kernel_name = ['Edge Detection#1', 'Edge Detection#2', 
-               'Bottom Sobel', 'Top Sobel', 
-               'Left Sobel', 'Right Sobel']
-figure, axis = plt.subplots(2,3, figsize=(6,5))
-for kernel, name, ax in zip(kernels, kernel_name, axis.flatten()):
-    conv_im1 = convolve2d(my_dog_gray, 
-                           kernel[::-1, ::-1]).clip(0,1)
-    ax.imshow(abs(conv_im1), cmap='gray')
-    ax.set_title(name)
 
-# plt.imshow(my_dog)
-plt.show()
+test_img = cv.imread('image.png')
+kernel = np.array([[-1, -1, -1], 
+                   [-1, 8, -1],
+                   [-1, -1, -1]])
+# gray_test = rgb2gray(img_as_float(test_img))
+gray_test = cv.cvtColor(test_img, cv.COLOR_BGR2GRAY)
+gray_test = cv.equalizeHist(gray_test)
+conv_test = abs(convolve2d(img_as_float(gray_test), kernel).clip(0,1))
+conv_test = img_as_ubyte(conv_test)
+cv.imshow("test", conv_test)
+cv.waitKey(0)
